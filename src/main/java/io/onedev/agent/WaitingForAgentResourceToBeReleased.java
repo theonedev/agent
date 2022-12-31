2e0859d3a04d57d9116846c63447ce04b1ca3e1b
Robin Shen <robin@onedev.io>
2022-12-31 11:00:33 +0800

    Bump version

---
+++ src/main/java/io/onedev/agent/WaitingForAgentResourceToBeReleased.java
@@ -0,0 +1,9 @@
+package io.onedev.agent;
+
+import java.io.Serializable;
+
+public class WaitingForAgentResourceToBeReleased implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
