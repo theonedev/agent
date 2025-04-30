c58b02ba44450c2edc9a5c48d01bfa7e8a816159
Robin Shen <robin@onedev.io>
2025-04-30 23:05:22 +0800

    chore: Show date/time in browser timezone for server/agent log

--- src/main/java/io/onedev/agent/job/LogRequest.java
+++ src/main/java/io/onedev/agent/job/LogRequest.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +16,8 @@ import io.onedev.commons.utils.FileUtils;
 public class LogRequest implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS");
 
 	public static List<String> readLog(File logFile) {
     	List<String> lines = new ArrayList<>();
@@ -30,4 +36,34 @@ public class LogRequest implements Serializable {
     	return lines;
 	}
 	
+	public static List<String> toZoneId(List<String> logLines, ZoneId zoneId) {
+		var zonedLogLines = new ArrayList<String>();
+		for (var logLine: logLines) {
+			if (zoneId.equals(ZoneId.systemDefault())) {
+				zonedLogLines.add(logLine);
+			} else {
+				int secondSpaceIndex = -1;
+				if (logLine.length() > 0) {
+					int firstSpaceIndex = logLine.indexOf(' ');
+					if (firstSpaceIndex != -1 && firstSpaceIndex + 1 < logLine.length()) 
+						secondSpaceIndex = logLine.indexOf(' ', firstSpaceIndex + 1);
+				}
+				if (secondSpaceIndex != -1) {						
+					try {
+						var dateString = logLine.substring(0, secondSpaceIndex);				
+						var localDate = LocalDateTime.parse(dateString, dateFormatter);
+						var zonedDate = localDate.atZone(ZoneId.systemDefault());
+						dateString = zonedDate.withZoneSameInstant(zoneId).format(dateFormatter);
+						zonedLogLines.add(dateString + logLine.substring(secondSpaceIndex));
+					} catch (DateTimeParseException ignored) {
+						zonedLogLines.add(logLine);
+					}
+				} else {
+					zonedLogLines.add(logLine);
+				}
+			}
+		}
+		return zonedLogLines;
+	}
+
 }
